196e0e3e5ec13bdafdd49cc08f6aebcde57b7e47
emiliano orsini <emiorsini20@hotmail.com>
2023-06-05 23:21:00 -0300

    modificacion para pre entrega 2

--- main.js
+++ main.js
@@ -2,7 +2,24 @@ let numero1 = parseInt(prompt("establesca cantidad de goles:"))
 let numer2 = parseInt(prompt("establesca cantidad de asistencias:"))
 let numero3 = parseInt(prompt("establesca cantidd de partidos jugados:"))
 
-function promedioParticipacionDeGolesPorPartido(goles, asistencias, partidos) {
+class topPromedios {
+    constructor(nombreJugador, promedioJugador){
+        this.nombre = nombreJugador
+        this.promedio = promedioJugador
+    }
+}
+
+const top5 = new topPromedios("Cristiano", 0.75)
+
+const top4 = new topPromedios("Eusebio", 0.81)
+
+const top3 = new topPromedios("Messi", 0.89)
+
+const top2 = new topPromedios("Seeler", 0.92)
+
+const top1 = new topPromedios("Muller", 0.94)
+
+function calcularPromedio(goles, asistencias, partidos) {
 
     if (!goles) {
         alert("Ingrese cantidad de goles!")
@@ -19,15 +36,36 @@ function promedioParticipacionDeGolesPorPartido(goles, asistencias, partidos) {
         return false
     }
     
-    if (promedio > 0.80) {
-        alert("top 10 futboilistas con mejor promedio")
+    let promedio = (goles + asistencias) / partidos
+
+    if (promedio > 0.70) {
+        alert("Entras en el top 5 futboilistas con mejor promedio")
     } else {
-        alert("no entra dentro del top 10 futbolistas con mejor promedio")
+        alert("no entra dentro del top 5 futbolistas con mejor promedio")
+    }
+   
+    if ((promedio > 0.75) && (promedio < 0.79)) {
+        alert("Felicidades!! te ubicas en el top 5 superando a" + this.nombre + "que su poromedio es de" + this.promedio)
     }
 
-    let promedio = (goles + asistencias) / partidos
+    if ((promedio > 0.79) && (promedio < 0.81)){
+        alert("Felicidades!! te ubicas en el top 4 superando a" + this.nombre + "que su poromedio es de" + this.promedio)
+    }
+    
+    if ((promedio > 0.81) && (promedio < 0.89)){
+        alert("Felicidades!! te ubicas en el top 3 superando a" + this.nombre + "que su poromedio es de" + this.promedio)
+    }
+
+    if ((promedio > 0.89) && (promedio < 0.92)){
+        alert("Felicidades!! te ubicas en el top 2 superando a" + this.nombre + "que su poromedio es de" + this.promedio)
+    }
+
+    if (promedio > 0.94){
+        alert("Felicidades!! te ubicas en el top 1 superando a" + this.nombre + "que su poromedio es de" + this.promedio)
+    }
+
     console.log(promedio)
     alert("el promedio de participacion en goles por partidos es de " + promedio.toFixed(2))
 }
 
-promedioParticipacionDeGolesPorPartido(numero1, numer2, numero3)
+calcularPromedio(numero1, numer2, numero3)
